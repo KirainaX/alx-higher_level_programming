711a72b347d703ac07a1056712637a51d8621156
KirainaX <bouzridazakaria@email.com>
2023-12-06 18:01:23 +0100

    free palestine

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,3 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-
-'''
-def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary.keys()):
-        print("{}: {}".format(i, a_dictionary[i]))
-'''
