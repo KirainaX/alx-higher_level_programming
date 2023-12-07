ecae0ea8222f8bd796b7f70712f6b991c4831a71
KirainaX <bouzridazakaria@email.com>
2023-12-07 09:27:14 +0100

    free palestine

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,22 +4,8 @@ def roman_to_int(roman_string):
         return 0
     total = 0
     num = 0
-    digits = {'I': 1, 'V': 5}
-
-
-roman_to_int = __import__('12-roman_to_int').roman_to_int
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        num = digits[i]
+        total += num if total < num * 5 else -num
+    return total
