6de2b30d67396a7a8152aa07942c7ec2865de355
KirainaX <bouzridazakaria@email.com>
2023-12-18 17:28:19 +0100

    free palestine

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    
-
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print("Exception:", err)
+        return False
