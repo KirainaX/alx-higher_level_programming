2fff56378d02600ab13c068b110e7b5bbfe8b97e
KirainaX <bouzridazakaria@email.com>
2023-12-18 12:04:02 +0100

    free palestine

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,23 +2,8 @@
 def safe_print_integer(value):
     try:
         while isinstance(value, int):
-            print("{:d}".format(value))
+            print("{:d}".format(value), end="")
+            print("")
             return True
     except (TypeError, ValueError):
         return False
-
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
