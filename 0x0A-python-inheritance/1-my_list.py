71fab6a05437a2df5b215f2d10ae4e161f0e6d5a
KirainaX <bouzridazakaria@email.com>
2024-01-08 11:40:40 +0100

    free palestine

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+'''Module for MyList class.'''
+
+
 class MyList(list):
-
+    '''Custom MyList class.'''
     def print_sorted(self):
+        '''Method for printing sorted list.'''
         print(sorted(self))
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
